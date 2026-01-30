3640ef5bc7f0bcb121cee72bf65a6415f3c0e937
Nitish S Hanchinagoudra <Nitish.S.Hanchinagoudra@ibm.com>
2026-01-30 21:24:43 +0530

    test: Remove existing resource test and its associated Terraform configurations.

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -2,18 +2,11 @@
 package test
 
 import (
-	"fmt"
-	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/common"
 	"github.com/terraform-ibm-modules/ibmcloud-terratest-wrapper/testhelper"
-
-	"github.com/gruntwork-io/terratest/modules/files"
-	"github.com/gruntwork-io/terratest/modules/random"
-	"github.com/gruntwork-io/terratest/modules/terraform"
-	"github.com/stretchr/testify/require"
 )
 
 // Use existing resource group
@@ -72,39 +65,10 @@ func TestRunUpgradeExample(t *testing.T) {
 	}
 }
 
-func setupTerraform(t *testing.T, prefix, realTerraformDir, region string) *terraform.Options {
-	tempTerraformDir, err := files.CopyTerraformFolderToTemp(realTerraformDir, prefix)
-	require.NoError(t, err, "Failed to create temporary Terraform folder")
-
-	existingTerraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
-		TerraformDir: tempTerraformDir,
-		Vars: map[string]interface{}{
-			"prefix":         prefix,
-			"region":         region,
-			"resource_group": resourceGroup,
-		},
-		// Set Upgrade to true to ensure latest version of providers and modules are used by terratest.
-		// This is the same as setting the -upgrade=true flag with terraform.
-		Upgrade: true,
-	})
-
-	terraform.WorkspaceSelectOrNew(t, existingTerraformOptions, prefix)
-	_, err = terraform.InitAndApplyE(t, existingTerraformOptions)
-	require.NoError(t, err, "Init and Apply of temp existing resource failed")
-
-	return existingTerraformOptions
-}
-func TestRunAdvancedExampleExistingInstance(t *testing.T) {
+func TestRunAdvancedExample(t *testing.T) {
 	t.Parallel()
 
-	prefix := fmt.Sprintf("brs-%s", strings.ToLower(random.UniqueId()))
-	options := setupOptions(t, "brs-adv-import", advancedExampleDir)
-	existingTerraformOptions := setupTerraform(t, prefix, "./resources", options.Region)
-	options.TerraformVars = map[string]interface{}{
-		"prefix":           prefix,
-		"brs_instance_crn": terraform.Output(t, existingTerraformOptions, "brs_instance_crn"),
-		"resource_group":   resourceGroup,
-	}
+	options := setupOptions(t, "brs-adv", advancedExampleDir)
 
 	output, err := options.RunTestConsistency()
 	assert.Nil(t, err, "This should not have errored")
