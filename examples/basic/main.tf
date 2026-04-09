ffec674ff0adc74b4d77d4d2f8a510f8c095f228
aashiq.jacob@ibm.com <aashiq.jacob@ibm.com>
2026-04-09 13:40:08 +0530

    test

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -42,5 +42,17 @@ module "brs" {
       duration = 4
       unit     = "Weeks"
     }
+    # See, https://github.com/IBM-Cloud/terraform-provider-ibm/issues/6738
+    # blackout_window = [{
+    #   day = "Sunday"
+    #   start_time = {
+    #     hour      = 2
+    #     minute    = 0
+    #   }
+    #   end_time = {
+    #     hour      = 6
+    #     minute    = 0
+    #   }
+    # }]
   }]
 }
